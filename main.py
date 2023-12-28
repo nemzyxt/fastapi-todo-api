fd43f06656143c579e2e0f0f66f85a4640f4a5e8
Nemuel Wainaina <nemuelwainaina@proton.me>
2023-12-28 21:51:04 +0300

    Define API routes

--- main.py
+++ main.py
@@ -2,6 +2,22 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get('/')
-def index():
-    return {'message':'hello world'}
+@app.post('/todo')
+def create_todo():
+    pass
+
+@app.get('/todo')
+def get_todos():
+    pass
+
+@app.get('/todo/{id}')
+def get_todo():
+    pass
+
+@app.put('/todo/{id}')
+def update_todo():
+    pass
+
+@app.delete('/todo/{id}')
+def delete_todo():
+    pass
